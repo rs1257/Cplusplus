78f80fa3fb0948ebe5401680c9076f79225ffcbd
ryansmith300082 <ryansmith300082@gmail.com>
2017-02-24 21:51:51 +0000

    add headers

--- Game/Game/Armor.cpp
+++ Game/Game/Armor.cpp
@@ -1,10 +1,30 @@
 #include <vector>
 
-typedef struct Armor {
-	int defense;
-	int slots;
-	int used_slots;
-	int sell_cost;
-	std::vector<int> skills; //identified by IDs.
-	std::vector<int> materials; //materials identified by ids
-} Armor;
+namespace Protection {
+	class Armor {
+		
+		private:
+			int ID;
+			int defense;
+			int slots;
+			int used_slots;
+			int sell_cost;
+			
+			std::vector<int> skills; //identified by IDs.
+			std::vector<int> materials; //materials identified by ids
+
+		Armor(int ID, int defense, int slots, int used_slots, int sell_cost,
+			std::vector<int> skills, std::vector<int> materials) {
+			Armor::ID = ID;
+			Armor::defense = defense;
+			Armor::slots = slots;
+			Armor::used_slots = used_slots;
+			Armor::sell_cost = sell_cost;
+			Armor::skills = skills;
+			Armor::materials = materials;
+		}
+	};
+}
+//std::vector<Armor> armor_list;
+
+
--- Game/Game/Main.cpp
+++ Game/Game/Main.cpp
@@ -1,6 +1,10 @@
 #include <String>
 #include <vector>
+#include <iostream>
+#include "Armor.cpp"
 
 int main() {
+	Armor a = Armor(2, 1, 1, 1, 1, { 1 }, { 1,32 });
+	std::cout << a.ID;
 	return 0;
 }
