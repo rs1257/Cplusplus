3fe16a4fefc2d45d92b68e2ca7652d02bd87b4d0
ryansmith300082 <ryansmith300082@gmail.com>
2017-02-25 16:02:51 +0000

    simple problems practice

---
+++ Practice/Practice/Practice/Main.cpp
@@ -0,0 +1,9 @@
+#include "Practice.h"
+
+#include <iostream>
+
+int main()
+{
+	//for testing functions in other files
+	return 0;
+}
---
+++ Practice/Practice/Practice/Practice.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+#include <vector>
+//string header file and namespace are already included for you
+std::string pattern(int n);
+
+long queueTime(std::vector<int> customers, int n);
+
+int RecursiveSum(int number, int sum);
+
+int sumTriangularNumbers(int n);
