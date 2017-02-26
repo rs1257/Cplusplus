af1d84ddaca0bd853f11246e6edb53e8016447f2
ryansmith300082 <ryansmith300082@gmail.com>
2017-02-26 10:47:54 +0000

    removed namespace

--- Game/Game/Armor.cpp
+++ Game/Game/Armor.cpp
@@ -1,30 +1,27 @@
 #include <vector>
 
-namespace Protection {
-	class Armor {
+class Armor {
 		
-		private:
-			int ID;
-			int defense;
-			int slots;
-			int used_slots;
-			int sell_cost;
+	private:
+		int ID;
+		int defense;
+		int slots;
+		int used_slots;
+		int sell_cost;
 			
-			std::vector<int> skills; //identified by IDs.
-			std::vector<int> materials; //materials identified by ids
+		std::vector<int> skills; //identified by IDs.
+		std::vector<int> materials; //materials identified by ids
 
-		Armor(int ID, int defense, int slots, int used_slots, int sell_cost,
-			std::vector<int> skills, std::vector<int> materials) {
-			Armor::ID = ID;
-			Armor::defense = defense;
-			Armor::slots = slots;
-			Armor::used_slots = used_slots;
-			Armor::sell_cost = sell_cost;
-			Armor::skills = skills;
-			Armor::materials = materials;
-		}
-	};
-}
-//std::vector<Armor> armor_list;
+	Armor(int ID, int defense, int slots, int used_slots, int sell_cost,
+		std::vector<int> skills, std::vector<int> materials) {
+		Armor::ID = ID;
+		Armor::defense = defense;
+		Armor::slots = slots;
+		Armor::used_slots = used_slots;
+		Armor::sell_cost = sell_cost;
+		Armor::skills = skills;
+		Armor::materials = materials;
+	}
+};
 
 
